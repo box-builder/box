94d2acdd3b92baca6b530bc1d99e2c17030c4b82
Erik Hollensbe <github@hollensbe.org>
2016-10-11 14:17:30 -0700

    simplify dockerfile
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- dockerfile-example.rb
+++ dockerfile-example.rb
@@ -5,31 +5,7 @@ packages = "build-essential g++ git wget curl ruby bison flex"
 run "apt-get update"
 run "apt-get install -y #{packages}"
 
-tag "erikh/box:packages"
-
 env "GOPATH" => "/go"
-
-gopaths = [
-  "github.com/docker/engine-api",
-  "github.com/docker/distribution/reference",
-  "github.com/docker/go-connections/nat",
-  "github.com/docker/go-units",
-  "golang.org/x/net/context",
-  "github.com/Sirupsen/logrus",
-  "github.com/opencontainers/runc/libcontainer/user",
-]
-
-gopaths.each { |gopath| run "go get -v -d #{gopath}" }
-
-run %q[
-  mkdir /go/src/github.com/mitchellh && \
-  cd /go/src/github.com/mitchellh && \
-  git clone https://github.com/mitchellh/go-mruby && \
-  cd go-mruby && \
-  cd /go/src/github.com/mitchellh/go-mruby && \
-  make &&
-  cp libmruby.a /root
-]
 
 tag "erikh/box:prereqs"
 
