787497e935e87ed9f6cf6f4061e389929f22eea4
Erik Hollensbe <github@hollensbe.org>
2016-10-11 20:55:37 -0700

    move container file-reading routine to funciton
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- builder/builder_test.go
+++ builder/builder_test.go
@@ -52,37 +52,7 @@ copy "%s", "/test1.rb"
 
 	c.Assert(err, IsNil)
 
-	b.config.Cmd = []string{"cat /test1.rb"}
-	id, err := b.createEmptyContainer()
-	c.Assert(err, IsNil)
-	resp, err := b.client.ContainerAttach(context.Background(), id, types.ContainerAttachOptions{Stream: true, Stdout: true, Stdin: true})
-	c.Assert(err, IsNil)
-
-	err = b.client.ContainerStart(context.Background(), id, types.ContainerStartOptions{})
-	c.Assert(err, IsNil)
-
-	buf := new(bytes.Buffer)
-
-	n, err := io.Copy(buf, resp.Reader)
-	c.Assert(err, IsNil)
-	c.Assert(n, Not(Equals), 0)
-
-	nr := bufio.NewReader(buf)
-	result := []byte{}
-
-	for err == nil {
-		var inner []byte
-		inner, err = nr.ReadBytes('\n')
-		if len(inner) >= 2 && inner[len(inner)-2] == '\r' {
-			inner = append(inner[:len(inner)-2], '\n')
-		}
-
-		result = append(result, inner...)
-	}
-
-	status, err := b.client.ContainerWait(context.Background(), id)
-	c.Assert(err, IsNil)
-	c.Assert(status, Equals, 0)
+	result := readContainerFile(c, b, "/test1.rb")
 
 	content, err := ioutil.ReadFile(testpath)
 	c.Assert(err, IsNil)
@@ -122,4 +92,40 @@ tag "flattened"
 	c.Assert(err, IsNil)
 
 	c.Assert(len(inspect.RootFS.Layers), Equals, 1)
+}
+
+func readContainerFile(c *C, b *Builder, fn string) []byte {
+	b.config.Cmd = []string{"cat " + fn}
+	id, err := b.createEmptyContainer()
+	c.Assert(err, IsNil)
+	resp, err := b.client.ContainerAttach(context.Background(), id, types.ContainerAttachOptions{Stream: true, Stdout: true, Stdin: true})
+	c.Assert(err, IsNil)
+
+	err = b.client.ContainerStart(context.Background(), id, types.ContainerStartOptions{})
+	c.Assert(err, IsNil)
+
+	buf := new(bytes.Buffer)
+
+	n, err := io.Copy(buf, resp.Reader)
+	c.Assert(err, IsNil)
+	c.Assert(n, Not(Equals), 0)
+
+	nr := bufio.NewReader(buf)
+	result := []byte{}
+
+	for err == nil {
+		var inner []byte
+		inner, err = nr.ReadBytes('\n')
+		if len(inner) >= 2 && inner[len(inner)-2] == '\r' {
+			inner = append(inner[:len(inner)-2], '\n')
+		}
+
+		result = append(result, inner...)
+	}
+
+	status, err := b.client.ContainerWait(context.Background(), id)
+	c.Assert(err, IsNil)
+	c.Assert(status, Equals, 0)
+
+	return result
 }
