9de7df7910a86b24483cfd15e701ca169c9b1068
Erik Hollensbe <github@hollensbe.org>
2016-10-12 23:19:56 +0000

    New build system based on box; uses build.rb to create both release and test images.
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- Makefile
+++ Makefile
@@ -5,9 +5,15 @@ vendor:
 	cd vendor/github.com/mitchellh/go-mruby && make clean all
 	cp vendor/github.com/mitchellh/go-mruby/libmruby.a .
 
+bootstrap:
+	docker build -t box-bootstrap .
+
 test:
-	docker build -t box .
-	docker run -it --privileged --rm -it box make docker-test
+	docker run -v /var/run/docker.sock:/var/run/docker.sock -i box-bootstrap < build.rb
+	docker run -it --privileged --rm -it box-test make docker-test
+
+release:
+	docker run -v /var/run/docker.sock:/var/run/docker.sock -e RELEASE=1 -i box-bootstrap < build.rb
 
 docker-test:
 	bash docker-test.sh
---
+++ build.rb
@@ -0,0 +1,42 @@
+from "golang"
+
+packages = %w[
+  build-essential
+  g++
+  git
+  wget
+  curl
+  ruby
+  bison
+  flex
+  iptables
+  psmisc
+]
+
+run "apt-get update"
+run "apt-get install -y #{packages.join(" ")}"
+env "GOPATH" => "/go"
+
+if getenv("RELEASE") == ""
+  run "wget https://get.docker.com/builds/Linux/x86_64/docker-1.12.1.tgz"
+  run "tar -xpf docker-1.12.1.tgz --strip-components=1 -C /usr/bin/"
+  run "rm docker-1.12.1.tgz"
+  copy "dind", "/dind"
+end
+
+copy ".", "/go/src/github.com/erikh/box"
+run "cd /go/src/github.com/erikh/box && make"
+
+if getenv("RELEASE") != ""
+  run "mv /go/bin/box /box"
+  entrypoint "/box"
+  run "apt-get purge -y #{packages.join(" ")}"
+  run "apt-get autoclean"
+  run "rm -rf /usr/local /go /var/cache/apt /var/lib/apt"
+  flatten
+  tag "erikh/box:latest"
+else
+  workdir "/go/src/github.com/erikh/box"
+  entrypoint "/dind"
+  tag "box-test"
+end
