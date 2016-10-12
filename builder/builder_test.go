59f24ec436d8c32a6cbda8035b1d1240a0fafa80
Erik Hollensbe <github@hollensbe.org>
2016-10-11 20:59:32 -0700

    move utility functions for tests into util_test.go
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- builder/builder_test.go
+++ builder/builder_test.go
@@ -1,16 +1,12 @@
 package builder
 
 import (
-	"bufio"
 	"bytes"
 	"context"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"path/filepath"
 	. "testing"
-
-	"github.com/docker/engine-api/types"
 
 	. "gopkg.in/check.v1"
 )
@@ -25,16 +21,6 @@ var _ = Suite(&builderSuite{})
 
 func TestBuilder(t *T) {
 	TestingT(t)
-}
-
-func runBuilder(script string) (*Builder, error) {
-	b, err := NewBuilder()
-	if err != nil {
-		return nil, err
-	}
-
-	_, err = b.Run(script)
-	return b, err
 }
 
 func (bs *builderSuite) TestSamePull(c *C) {
@@ -92,40 +78,4 @@ tag "flattened"
 	c.Assert(err, IsNil)
 
 	c.Assert(len(inspect.RootFS.Layers), Equals, 1)
-}
-
-func readContainerFile(c *C, b *Builder, fn string) []byte {
-	b.config.Cmd = []string{"cat " + fn}
-	id, err := b.createEmptyContainer()
-	c.Assert(err, IsNil)
-	resp, err := b.client.ContainerAttach(context.Background(), id, types.ContainerAttachOptions{Stream: true, Stdout: true, Stdin: true})
-	c.Assert(err, IsNil)
-
-	err = b.client.ContainerStart(context.Background(), id, types.ContainerStartOptions{})
-	c.Assert(err, IsNil)
-
-	buf := new(bytes.Buffer)
-
-	n, err := io.Copy(buf, resp.Reader)
-	c.Assert(err, IsNil)
-	c.Assert(n, Not(Equals), 0)
-
-	nr := bufio.NewReader(buf)
-	result := []byte{}
-
-	for err == nil {
-		var inner []byte
-		inner, err = nr.ReadBytes('\n')
-		if len(inner) >= 2 && inner[len(inner)-2] == '\r' {
-			inner = append(inner[:len(inner)-2], '\n')
-		}
-
-		result = append(result, inner...)
-	}
-
-	status, err := b.client.ContainerWait(context.Background(), id)
-	c.Assert(err, IsNil)
-	c.Assert(status, Equals, 0)
-
-	return result
 }
---
+++ builder/util_test.go
@@ -0,0 +1,58 @@
+package builder
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"io"
+
+	. "gopkg.in/check.v1"
+
+	"github.com/docker/engine-api/types"
+)
+
+func runBuilder(script string) (*Builder, error) {
+	b, err := NewBuilder()
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = b.Run(script)
+	return b, err
+}
+
+func readContainerFile(c *C, b *Builder, fn string) []byte {
+	b.config.Cmd = []string{"cat " + fn}
+	id, err := b.createEmptyContainer()
+	c.Assert(err, IsNil)
+	resp, err := b.client.ContainerAttach(context.Background(), id, types.ContainerAttachOptions{Stream: true, Stdout: true, Stdin: true})
+	c.Assert(err, IsNil)
+
+	err = b.client.ContainerStart(context.Background(), id, types.ContainerStartOptions{})
+	c.Assert(err, IsNil)
+
+	buf := new(bytes.Buffer)
+
+	n, err := io.Copy(buf, resp.Reader)
+	c.Assert(err, IsNil)
+	c.Assert(n, Not(Equals), 0)
+
+	nr := bufio.NewReader(buf)
+	result := []byte{}
+
+	for err == nil {
+		var inner []byte
+		inner, err = nr.ReadBytes('\n')
+		if len(inner) >= 2 && inner[len(inner)-2] == '\r' {
+			inner = append(inner[:len(inner)-2], '\n')
+		}
+
+		result = append(result, inner...)
+	}
+
+	status, err := b.client.ContainerWait(context.Background(), id)
+	c.Assert(err, IsNil)
+	c.Assert(status, Equals, 0)
+
+	return result
+}
