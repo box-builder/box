2200e07decb59c4c1e6c71dcbcc1904f29a45381
Erik Hollensbe <github@hollensbe.org>
2017-04-14 00:36:45 -0700

    repl: rewrite; handle several situations in non-tty mode
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- repl/repl.go
+++ repl/repl.go
@@ -6,6 +6,9 @@ import (
 	"io"
 	"os"
 	"strings"
+	"syscall"
+
+	gosig "os/signal"
 
 	gm "github.com/mitchellh/go-mruby"
 
@@ -80,6 +83,26 @@ func NewRepl(omit []string, log *logger.Logger) (*Repl, error) {
 	return &Repl{readline: rl, evaluator: e, globals: globals}, nil
 }
 
+func (r *Repl) handleError(line string, err error) bool {
+	if err == io.EOF {
+		os.Exit(0)
+	}
+
+	if _, interrupted := err.(*readline.InterruptError); interrupted || err.Error() == "Interrupt" {
+		if line != "" {
+			r.readline.SetPrompt(normalPrompt)
+		} else {
+			fmt.Println("You can press ^D or type \"quit\" or \"exit\" to exit the shell")
+		}
+
+		return true
+	} else if err != nil {
+		fmt.Printf("+++ Error %#v\n", err)
+		os.Exit(1)
+	}
+	return false
+}
+
 // Loop runs the loop. Returns nil on io.EOF, otherwise errors are forwarded.
 func (r *Repl) Loop() error {
 	defer func() {
@@ -87,67 +110,85 @@ func (r *Repl) Loop() error {
 			fmt.Printf("Aborting due to interpreter error: %v\n", err)
 			os.Exit(2)
 		}
+		r.readline.Close()
 	}()
 
 	var line string
 	var stackKeep int
 
+	signals := make(chan os.Signal, 2)
+	// in no-tty mode, a literal ^C would be sent directly to the signal handler
+	// and not the readline reader, causing a bug where the repl would get stuck.
+	// So we install a signal handler just to trap interrupt.
+	if !r.globals.TTY {
+		gosig.Notify(signals, syscall.SIGINT)
+		defer gosig.Stop(signals)
+	}
+
+	lineChan := make(chan string, 1)
+	errChan := make(chan error, 1)
+	syncChan := make(chan struct{})
+	go func() {
+		for {
+			tmp, err := r.readline.Readline()
+			if err != nil {
+				errChan <- err
+			} else {
+				lineChan <- tmp
+			}
+			<-syncChan
+		}
+	}()
+
 	for {
-		tmp, err := r.readline.Readline()
-		if err == io.EOF {
-			return nil
+		var (
+			cancel context.CancelFunc
+			cont   bool
+		)
+
+		if cancel != nil {
+			cancel()
 		}
 
-		if _, ok := err.(*readline.InterruptError); ok || err != nil {
-			if line != "" {
-				r.readline.SetPrompt(normalPrompt)
-			} else {
-				fmt.Println("You can press ^D or type \"quit\" or \"exit\" to exit the shell")
-			}
+		r.globals.Context, cancel = context.WithCancel(context.Background())
+		signal.Handler.AddFunc(cancel)
 
-			line = ""
+		line, cont = r.readChannels(line, lineChan, errChan, signals)
+
+		if cont {
+			syncChan <- struct{}{}
 			continue
 		}
 
-		if err != nil {
-			fmt.Printf("+++ Error %#v\n", err)
-			os.Exit(1)
-		}
-
-		line += tmp + "\n"
-
 		checkQuit(line)
-
-		ctx, cancel := context.WithCancel(context.Background())
-		r.globals.Context = ctx
-		signal.Handler.AddFunc(cancel)
 
 		newKeep, err := r.evaluator.RunCode(line, stackKeep)
 		if err != nil {
 			switch err.(type) {
-			case gm.ParserError:
+			case *gm.ParserError:
 				if newKeep == stackKeep {
 					r.readline.SetPrompt(multilinePrompt)
+					syncChan <- struct{}{}
 					continue
 				}
-			default:
-				goto reset
 			}
 		}
 
 		stackKeep = newKeep
-
-	reset:
 		line = ""
+
 		r.readline.SetPrompt(normalPrompt)
 		if err != nil {
 			fmt.Printf("+++ Error: %v\n", err)
+			syncChan <- struct{}{}
 			continue
 		}
 
 		if r.evaluator.Result().Value != "" {
 			fmt.Println(r.evaluator.Result().Value)
 		}
+
+		syncChan <- struct{}{}
 	}
 }
 
@@ -158,4 +199,31 @@ func checkQuit(line string) {
 	case "exit":
 		os.Exit(0)
 	}
+}
+
+func (r *Repl) readChannels(line string, lineChan <-chan string, errChan <-chan error, signals <-chan os.Signal) (string, bool) {
+	var (
+		tmp string
+		err error
+	)
+
+	select {
+	case err = <-errChan:
+		if r.handleError(line, err) {
+			return "", true
+		}
+	case <-signals:
+		fmt.Println("Statement canceled.")
+
+		select {
+		case err := <-errChan:
+			r.handleError(line, err) // the return value isn't necessary here.
+		default:
+		}
+
+		return "", true
+	case tmp = <-lineChan:
+	}
+
+	return line + tmp + "\n", false
 }
